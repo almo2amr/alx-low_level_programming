85328b0936deb2ced59127a646b51c2d8b0546a7
almo2amr <moaamr10@gmial.com>
2023-03-16 14:17:35 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Piont
+ *
+ * description: a C program that prints a quote with printf function.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
