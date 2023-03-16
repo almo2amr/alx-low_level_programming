d2849cdea2a93a54ae77fb20ff970bffbe71c556
almo2amr <moaamr10@gmial.com>
2023-03-16 16:13:20 -0700

    final task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *  main - Entry Point
+ *
+ *  description: a c program that prints a qoute using write function
+ *  	write(int fd.const void *buf.size_t count)
+ *
+ *  Return: 1 (error)
+*/
+
+int main(void)
+{
+	writw(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	return (1);
+}
