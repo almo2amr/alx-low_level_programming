5f62e90efccdc61427070d9f9685b4af33eac144
almo2amr <moaamr10@gmial.com>
2023-03-16 16:22:30 -0700

    finish

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,13 @@
  *  main - Entry Point
  *
  *  description: a c program that prints a qoute using write function
- *  	write(int fd.const void *buf.size_t count)
+ *  write(int fd.const void *buf.size_t count)
  *
  *  Return: 1 (error)
 */
 
 int main(void)
 {
-	writw(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
