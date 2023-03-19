1f938c63501d24c6069727ec328b37fe732e0de4
almo2amr <moaamr10@gmial.com>
2023-03-19 11:33:44 -0700

    advance 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,12 +19,12 @@ int main(void)
 		{
 			for (ones = '0'; ones <= '9'; ones++)
 			{
-				if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
+				if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))/*eliminates repitition*/
 				{
 					putchar(hundreds);
 					putchar(tens);
 					putchar(ones);
-					if (!(ones == '9' && tens == '7'))/*adds comma and space*/
+					if (!(ones == '9' && hundreds == '7' && tens == '8'))/*adds comma and space*/
 					{
 						putchar(',');
 						putchar(' ');
