40a71b7ba6aa1afa7cba31931cd69537ec2f76bc
almo2amr <moaamr10@gmial.com>
2023-03-21 17:04:26 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry Point
- *
- * description: print alphabet utilizes on the _putchar function
+ * print_alphabet - utilizes on the _putchar function
  *	to print from a - z
  *
  * Return: Always 0 (Success)
