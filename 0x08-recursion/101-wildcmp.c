d4b89b04b457eb50ea4ee3bac609e373495e4f92
almo2amr <moaamr10@gmial.com>
2023-04-06 13:02:40 -0700

    advanced 2

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * *move_past_star - it repeats past asterisk
+ *
+ * @s2: pointer to second string that can contain WildCard
+ *
+ * Return: pointer past astersik
+*/
+char *move_past_star(char *s2)
+{
+	if (*s2 == '*')
+	{
+		return (move_past_star(s2 + 1));
+	}
+	else
+	{
+		return (s2);
+	}
+}
+
+/**
+ * inception - it converts magic into reality
+ *
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string that can contain WildCard
+ *
+ * Return: returns 1 if the strings can be considered identical
+ *	otherwise return 0
+*/
+int inception(char *s1, char *s2)
+{
+	int i = 0;
+
+	if (*s1 == 0)
+	{
+		return (0);
+	}
+	if (*s1 == *s2)
+	{
+		i += wildcmp(s1 + 1, s2 + 1);
+		i += inception(s1 + 1, s2);
+		return (i);
+	}
+}
+
+/**
+ * wildcmp - it is a function that compares two strings
+ *
+ * @s1: pointer to the first string
+ * @s2: pointer to the second string that can contain WildCard
+ *
+ * Return: returns 1 if the strings can be considered identical
+ *	otherwise return 0
+*/
+int wildcmp(char *s1, char *s2)
+{
+	int i = 0;
+
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		if (!*s1)
+			return (1);
+		return (wildcmp(s1 + 1, *s2 == '*' ? s2 : s2 + 1));
+	}
+	if (!*s1 || !s2)
+	{
+		return (0);
+	}
+	if (*s2 == '*')
+	{
+		s2 = move_past_star(s2);
+		if (!*s2)
+			return (1);
+		if (*s1 == *s2)
+			i += wildcmp(s1 + 1, s2 + 1);
+		i += inception(s1, s2);
+		return (!!i);
+	}
+	return (0);
+}
