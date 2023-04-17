084c5d3bb17ea797ea484b6344fdc1644f1aa4d0
almo2amr <moaamr10@gmial.com>
2023-04-17 14:59:39 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct (dog's information)
+ *
+ * @name: first member (the name of the dog)
+ * @age: second member (the age of the dog)
+ * @owner: third member (the name of the owner)
+ *
+ * description: a long dog struct (information)
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
